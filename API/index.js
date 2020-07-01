59b83f2146fe5dfefd87e489936fd2e049947966
Alejandro Upegui Betancur <alejandro.upegui@ias.com.co>
2020-07-01 16:32:27 -0500

    Documentation of the API using Swagger

--- API/index.js
+++ API/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
 const configure = require("../configure.js");
 const app = express();
 const user = require("../API/components/user/network");
 app.use(bodyParser.json());
+const swaggerDoc = require("../swagger.json");
 // Rutas
 app.use("/api/user", user);
 
-app;
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
 app.listen(configure.api.port, () => {
   console.log(`API intialized in port ${configure.api.port} `);
 });
